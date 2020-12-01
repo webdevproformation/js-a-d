e1fef55590dd10b6a78922c2ad39d63edd56a382
malik <malik.h@webdevpro.net>
2020-12-01 12:02:24 +0100

    correction

---
+++ jour2/04-data.js
@@ -0,0 +1,273 @@
+// https://www.json-generator.com/
+export let data = [
+    {
+      "_id": "5fc61d192a391aadbce62f6b",
+      "index": 0,
+      "guid": "1ab38501-1421-48c6-80cf-92f325db554f",
+      "isActive": true,
+      "balance": "$2,339.02",
+      "picture": "http://placehold.it/32x32",
+      "age": 33,
+      "eyeColor": "green",
+      "name": "Guzman Cardenas",
+      "gender": "male",
+      "company": "PATHWAYS",
+      "email": "guzmancardenas@pathways.com",
+      "phone": "+1 (830) 443-2017",
+      "address": "826 Christopher Avenue, Lacomb, Marshall Islands, 2435",
+      "about": "Voluptate quis veniam tempor ipsum in voluptate nulla reprehenderit occaecat eiusmod eiusmod in. Consequat deserunt non fugiat eiusmod nostrud eu fugiat. Enim in in reprehenderit occaecat incididunt excepteur dolor ut. Consectetur ea id occaecat nisi Lorem reprehenderit mollit. Officia minim non do reprehenderit quis non in amet sint. Commodo mollit sint Lorem dolore ut ex id adipisicing nisi proident sunt.\r\n",
+      "registered": "2014-08-06T03:02:35 -02:00",
+      "latitude": -71.497413,
+      "longitude": -97.579133,
+      "tags": [
+        "officia",
+        "anim",
+        "dolor",
+        "voluptate",
+        "commodo",
+        "elit",
+        "adipisicing"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Valdez Fischer"
+        },
+        {
+          "id": 1,
+          "name": "Lina Noble"
+        },
+        {
+          "id": 2,
+          "name": "Gentry Gillespie"
+        }
+      ],
+      "greeting": "Hello, Guzman Cardenas! You have 2 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "5fc61d195bcee84d3ffc97b7",
+      "index": 1,
+      "guid": "2ca6cfb8-2686-4daf-a8b3-12e2ed231c13",
+      "isActive": true,
+      "balance": "$2,638.04",
+      "picture": "http://placehold.it/32x32",
+      "age": 36,
+      "eyeColor": "green",
+      "name": "Blanca Bernard",
+      "gender": "female",
+      "company": "TERRAGEN",
+      "email": "blancabernard@terragen.com",
+      "phone": "+1 (984) 532-2031",
+      "address": "948 Portland Avenue, Convent, Vermont, 6213",
+      "about": "Ullamco reprehenderit consequat aute ad fugiat aliqua qui dolore deserunt Lorem nostrud officia aliquip. Officia dolor sit ullamco ut nisi do laboris ullamco eu magna amet. Sunt Lorem proident dolor ad ad velit pariatur aute quis consectetur tempor magna et. Labore nulla labore occaecat culpa occaecat nulla minim in. Proident ut quis excepteur cillum laborum sunt dolor aliquip in et proident cillum.\r\n",
+      "registered": "2014-10-09T04:30:06 -02:00",
+      "latitude": 16.408011,
+      "longitude": 46.207636,
+      "tags": [
+        "ea",
+        "magna",
+        "mollit",
+        "non",
+        "eiusmod",
+        "do",
+        "enim"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Cherry Harmon"
+        },
+        {
+          "id": 1,
+          "name": "Woodward Villarreal"
+        },
+        {
+          "id": 2,
+          "name": "Ollie Mcconnell"
+        }
+      ],
+      "greeting": "Hello, Blanca Bernard! You have 4 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "5fc61d19ee72f3dbc27da72b",
+      "index": 2,
+      "guid": "a789701a-19b4-4dbb-814d-0033a3d7daba",
+      "isActive": true,
+      "balance": "$2,083.87",
+      "picture": "http://placehold.it/32x32",
+      "age": 28,
+      "eyeColor": "green",
+      "name": "Beasley Walls",
+      "gender": "male",
+      "company": "CODACT",
+      "email": "beasleywalls@codact.com",
+      "phone": "+1 (993) 468-2111",
+      "address": "973 Highlawn Avenue, Celeryville, Palau, 4133",
+      "about": "Laborum id non duis laborum tempor mollit. Velit laborum velit occaecat aliquip dolore laboris esse. Lorem pariatur ad ipsum minim anim dolor labore. Exercitation occaecat nostrud culpa cupidatat minim commodo eiusmod eu ad duis quis. Ad id aliquip reprehenderit ullamco ut veniam esse reprehenderit incididunt. Minim ea aliqua dolore esse esse magna mollit ad.\r\n",
+      "registered": "2018-06-04T10:13:34 -02:00",
+      "latitude": -54.166438,
+      "longitude": 34.902206,
+      "tags": [
+        "ipsum",
+        "labore",
+        "adipisicing",
+        "ad",
+        "officia",
+        "dolore",
+        "cillum"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Silvia Cline"
+        },
+        {
+          "id": 1,
+          "name": "Jannie Joyce"
+        },
+        {
+          "id": 2,
+          "name": "Leigh Bolton"
+        }
+      ],
+      "greeting": "Hello, Beasley Walls! You have 8 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "5fc61d195e75ad0669c79c03",
+      "index": 3,
+      "guid": "60ba4319-7bf9-4f50-ab47-333929991817",
+      "isActive": true,
+      "balance": "$1,003.58",
+      "picture": "http://placehold.it/32x32",
+      "age": 24,
+      "eyeColor": "brown",
+      "name": "Beulah Morales",
+      "gender": "female",
+      "company": "XUMONK",
+      "email": "beulahmorales@xumonk.com",
+      "phone": "+1 (961) 518-3190",
+      "address": "729 Devon Avenue, Whitehaven, Arkansas, 6067",
+      "about": "Ea enim culpa do est velit ea veniam cupidatat fugiat voluptate cillum mollit proident. Reprehenderit reprehenderit pariatur officia dolore occaecat ex. Tempor aute cupidatat esse reprehenderit occaecat tempor nulla sint sit mollit eu do ullamco. Qui nostrud voluptate nisi amet enim ex deserunt aute in ullamco aliquip. Dolore qui incididunt magna adipisicing ex enim veniam anim reprehenderit sunt commodo sit. In do sit commodo laborum mollit sunt.\r\n",
+      "registered": "2016-02-15T08:23:50 -01:00",
+      "latitude": -41.678423,
+      "longitude": 66.215772,
+      "tags": [
+        "consectetur",
+        "adipisicing",
+        "enim",
+        "fugiat",
+        "cillum",
+        "esse",
+        "aliquip"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Francis Bright"
+        },
+        {
+          "id": 1,
+          "name": "Lucia Ball"
+        },
+        {
+          "id": 2,
+          "name": "Esperanza Hunter"
+        }
+      ],
+      "greeting": "Hello, Beulah Morales! You have 2 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "5fc61d19d557641f84a69646",
+      "index": 4,
+      "guid": "0f8e6780-0a45-472d-903c-55b3d6662504",
+      "isActive": false,
+      "balance": "$3,855.44",
+      "picture": "http://placehold.it/32x32",
+      "age": 31,
+      "eyeColor": "brown",
+      "name": "Mckee Avery",
+      "gender": "male",
+      "company": "EARBANG",
+      "email": "mckeeavery@earbang.com",
+      "phone": "+1 (856) 461-3254",
+      "address": "514 Mersereau Court, Hiko, New Mexico, 8280",
+      "about": "Nisi culpa nostrud aliqua aliqua proident minim cillum officia ut. Exercitation commodo commodo esse ullamco officia laboris pariatur. Culpa occaecat culpa aliquip sit voluptate duis labore minim est consectetur ex. Fugiat nulla dolor nisi enim est laboris proident ea amet duis est.\r\n",
+      "registered": "2019-10-22T05:20:36 -02:00",
+      "latitude": -66.606361,
+      "longitude": -118.152409,
+      "tags": [
+        "irure",
+        "pariatur",
+        "aute",
+        "dolor",
+        "officia",
+        "amet",
+        "in"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Wade Byrd"
+        },
+        {
+          "id": 1,
+          "name": "Bentley Buchanan"
+        },
+        {
+          "id": 2,
+          "name": "Lydia Crosby"
+        }
+      ],
+      "greeting": "Hello, Mckee Avery! You have 1 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "5fc61d19149f7e90e4dd6633",
+      "index": 5,
+      "guid": "bcd29567-8bda-4fba-98b6-29a9b8f76e25",
+      "isActive": false,
+      "balance": "$3,403.14",
+      "picture": "http://placehold.it/32x32",
+      "age": 21,
+      "eyeColor": "blue",
+      "name": "Juarez Steele",
+      "gender": "male",
+      "company": "TROLLERY",
+      "email": "juarezsteele@trollery.com",
+      "phone": "+1 (835) 435-3668",
+      "address": "137 Ide Court, Weeksville, Washington, 8594",
+      "about": "Elit exercitation tempor minim cupidatat pariatur tempor in pariatur laborum elit et. Sit laborum minim magna aliqua velit cillum non ex pariatur anim ea ad. Sit fugiat sint duis sint veniam laboris. Eiusmod anim dolore amet dolor veniam mollit aute eiusmod ut.\r\n",
+      "registered": "2017-11-24T01:53:09 -01:00",
+      "latitude": 69.43219,
+      "longitude": 94.899126,
+      "tags": [
+        "elit",
+        "commodo",
+        "Lorem",
+        "nulla",
+        "dolore",
+        "culpa",
+        "sunt"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Lynne Winters"
+        },
+        {
+          "id": 1,
+          "name": "Viola Everett"
+        },
+        {
+          "id": 2,
+          "name": "Dana Whitney"
+        }
+      ],
+      "greeting": "Hello, Juarez Steele! You have 1 unread messages.",
+      "favoriteFruit": "apple"
+    }
+] ;
---
+++ jour2/04-traitement.js
@@ -0,0 +1,37 @@
+export function generateArticle(param){
+    let html = `
+        <article>
+            <h1>${param.name}</h1>
+            <img src="${param.picture}" alt="" >
+            <p>${param.about}</p>
+            <ul>
+                ${ param.tags.map( item => "<li>" + item + "</li>"  ) }
+            </ul>
+        </article>
+    `; // nouvelle manière de créer des chaines de caractères
+
+    console.log(html)
+}
+// ${ param.tags.map( function(item){ return "<li>" + item + "</li>" }  ) }
+
+// tableau 
+let a = [ 1 , 2 , 3 ];
+// vu que c'est une tableau => javascript va ajouter à cette variable des méthodes
+// objet natifs
+a.map( )
+
+// .map() => parcourir chacun des éléments  du tableau 
+// ajouter des valeurs en plus pour chaque item 
+// .map( ) prendre une déclaration de fonction comme paramètre 
+// callback 
+
+// document.getElementById("h1").addEventListener( "click" , () => {} )
+
+// a.map(function(){})
+// a.map(() => {} )
+// a.map(item =>  )
+
+//let a = [ 1 , 2 , 3 ];
+//let b = [ <li>1</li> , <li>2</li> , <li>3</li> ];
+
+// let b = a.map(function(item){ return "<li>"+item+"</li>" } );
