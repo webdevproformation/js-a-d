79757c7f0b2c97fa0c5ee2a0627d835ce908b036
malik <malik.h@webdevpro.net>
2020-12-03 16:39:18 +0100

    enonce

---
+++ jour4/11-hello.js
@@ -0,0 +1,17 @@
+console.log("je suis un script dans exécuter dans NodeJS");
+
+/*
+dans le dossier jour4 , créer le fichier 12-exo.js
+
+Il contient le tableau suivant ["lundi", "mardi", "mercredi"]
+
+1 afficher dans la terminal de Visual Studio  les informations suivantes :
+lundi
+mardi
+mercredi
+
+2 ajouter à la fin du tableau la valeur "jeudi"
+
+afficher la tableau en intégralité dans le terminal de Visual Studio 
+
+*/ 
