93021e96cc8f0060279e69a17bf0fe5b1d74e5b3
malik <malik.h@webdevpro.net>
2020-12-04 16:20:59 +0100

    enonce

--- jour5-ts/04-variable-avancee.ts
+++ jour5-ts/04-variable-avancee.ts
@@ -38,4 +38,22 @@ let data2 :Array<Article> = [
 ]
 
 // data2
+// cas pratique 
+/** 
+ créer le fichier 05-exo.ts
+*/
+// veuillez les typer les variables suivantes correctement :
 
+// liste1 = "pêche poire ananas";
+// liste2 = ["pêche" , "poire", "ananas"];
+// liste3 = ["pêche", 4, "poire", 3, "ananas" , 2];
+/* liste4 = {
+    items : ["pêche", 4, "poire", 3, "ananas" , 2],
+    budget : 40 ,
+    unite : "euros"
+};*/
+/* liste5 = [
+    {id: 1 , nom : "pêche" , quantite : 4},
+    {id: 2 , nom : "poire" , quantite : 3},
+    {id: 3 , nom : "ananas" , quantite : 2},
+];*/
