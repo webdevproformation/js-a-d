9d9339f6ba3d3019dcdb268994641b70915c25f3
malik <malik.h@webdevpro.net>
2020-12-04 14:16:08 +0100

    présentation de npm

--- jour5/08-exo.js
+++ jour5/08-exo.js
@@ -41,4 +41,52 @@ server.listen(3200, "localhost", function(){
     console.log("le serveur est démarré et ecoute le port 3200");
 }); // 3ème argument de listen() callback 
 
+// créer un nouveau dossier jour5-npm 
+// dans le terminal on va se positionnez 
+// Ctrl + C => arrêter le serveur
+// cd ../jour5-npm 
+// cls
 
+// node nom-de-fichier.js
+// en même temps que vous avez installé node.js 
+// aussi un autre outil npm 
+// npm --version
+// autre outil de ligne 
+// NPM : Node Package Manager = outil qui va permettre de télécharger 
+// module depuis un site internet npmjs.com
+// npm ......
+// téléchargement 
+// npmjs.com > moteur de recherche => express 
+
+// installer le module express => il a été codé par la communaute
+// créer des fichiers js => mis à disposition sur le site npmjs.com
+// télécharger et les utiliser dans un mini projet que l'on va réaliser nous meme 
+
+// package.json => npm init --yes
+// donne des indications sur l'ensemble des fichiers et des dépendances
+// du dossier === projet 
+
+// installer le module express 
+// npm i express
+// ?? que fait cette commande ??
+
+// 1  node_modules dans le dossier en cours
+// 2 télécharger le module ainsi que ses dépendances
+// express => js à lui même besoin d'autres modules
+
+// 3 crée le fichier package-lock.json
+// donne uene indication sur où a été téléchargé chaque module dans le dossier node_modules
+// version 
+
+// 4 modifié le fichier package.json
+// mis à jour de fichier 
+// àjouté dans le fichier.json
+
+/*
+ "dependencies": {
+    "express": "^4.17.1"
+  }
+  // si je veux faire fonctionner le projet jour5-npm
+  // je dois obligatoirement avoir express installé 
+  // si je ne l'ai pas => le projet ne peut pas fonctionner
+*/
